f23221c53f1ad07e31f209a5b6c2848dab7009cf
LEE01106 <ye81106@gmail.com>
2026-05-07 15:05:44 +0900

    Update complete.js

--- portone-server/api/payment/complete.js
+++ portone-server/api/payment/complete.js
@@ -1,3 +1,10 @@
+// 서버 코드(complete.js) 맨 위에 추가
+res.setHeader('Access-Control-Allow-Origin', '*'); 
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+if (req.method === 'OPTIONS') return res.status(200).end();
+
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
 
