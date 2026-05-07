d7c76d54e16ed8f550b8577e74a5702ec38e2014
LEE01106 <ye81106@gmail.com>
2026-05-07 14:39:42 +0900

    Update complete.js

--- portone-server/api/payment/complete.js
+++ portone-server/api/payment/complete.js
@@ -1,39 +1,40 @@
-{\rtf1\ansi\ansicpg949\cocoartf2821
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset129 AppleSDGothicNeo-Regular;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh15980\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+export default async function handler(req, res) {
+  // 1. POST 요청이 아니면 거절
+  if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
 
-\f0\fs24 \cf0 export default async function handler(req, res) \{\
-  if (req.method !== "POST") \{\
-    return res.status(405).send("Method Not Allowed")\
-  \}\
-\
-  const \{ paymentId \} = req.body\
-\
-  try \{\
-    const response = await fetch(\
-      `https://api.portone.io/payments/$\{paymentId\}`,\
-      \{\
-        headers: \{\
-          Authorization: "eiHhpDQ4MGKgxxdLdDe9wZefk6EOkVKXkAuNeu6Yc0t2wsy2kqX7d4FFUSVho9d5sZ5HiD8y5B8EvhFC",\
-        \},\
-      \}\
-    )\
-\
-    const data = await response.json()\
-\
-    if (data.status === "PAID") \{\
-      return res.status(200).json(\{ status: "PAID" \})\
-    \} else \{\
-      return res.status(200).json(\{ status: data.status \})\
-    \}\
-  \} catch (e) \{\
-    return res.status(500).send("
-\f1 \'b0\'cb\'c1\'f5
-\f0  
-\f1 \'bd\'c7\'c6\'d0
-\f0 ")\
-  \}\
-\}}
+  try {
+    const { paymentId } = req.body; 
+
+    // 2. 포트원 결제내역 단건조회 API 호출
+    const paymentResponse = await fetch(
+      `https://api.portone.io/payments/${encodeURIComponent(paymentId)}`,
+      {
+        headers: { 
+          // Vercel 환경변수에 등록한 이름과 똑같아야 함
+          Authorization: `PortOne ${process.env.PORTONE_API_SECRET}` 
+        },
+      }
+    );
+
+    if (!paymentResponse.ok) throw new Error("결제 조회 실패");
+    const payment = await paymentResponse.json();
+
+    // 3. 금액 검증 (나이키 운동화 가격 1000원 기준)
+    const orderAmount = 1000; 
+
+    if (orderAmount === payment.amount.total) {
+      switch (payment.status) {
+        case "PAID":
+          return res.status(200).json({ status: "PAID" });
+        case "VIRTUAL_ACCOUNT_ISSUED":
+          return res.status(200).json({ status: "READY" });
+        default:
+          return res.status(200).json({ status: payment.status });
+      }
+    } else {
+      return res.status(400).send("결제 금액 불일치");
+    }
+  } catch (e) {
+    return res.status(400).send(e.message);
+  }
+}
